5a6fdf3bcae9ef89fdfc82c114851aabd0fcdcb0
Matt <mphlamb@gmail.com>
2024-04-28 22:08:24 +0200

    formatting, added button

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,7 @@ const audioPlayer = document.querySelector('audio')
 
 
 
-// ? PLAY FUNCTION --------------------------------------------------------
+// ? PLAY FUNCTION ------------------------------------------------------------
 
 
 
@@ -25,6 +25,25 @@ getHtmlElement.addEventListener('click', function(event) {
 )
 
 
+// ? LAZY FUNCTION --------------------------------------------------------
+// PLAY EACH SOUND IN SEQUENCE BY CLICKING ONE BUTTON
+
+
+    let clickCount = 0
+const lazyPlayback = () => {
+    if(clickCount <15) {
+    clickCount ++
+    } else {
+        clickCount = 0
+    }
+    audioPlayer.src = daftSound[clickCount].soundToPlay
+    audioPlayer.play()
+    console.log(clickCount)
+}
+
+lazyButton = document.querySelector("#lazy-play")
+lazyButton.addEventListener("click", lazyPlayback)
+
 
 // ? RANDOMISE FUNCTION --------------------------------------------------------
 
@@ -33,6 +52,7 @@ const randomiseSample = () => {
 
     audioPlayer.src = daftSound[(Math.floor(Math.random() * 16))].soundToPlay
     audioPlayer.play()
+
 }
 
 randomiseButton = document.querySelector("#randomizer")
@@ -42,23 +62,22 @@ randomiseButton.addEventListener("click", randomiseSample)
 
 
 
-// ? ITERATE 8 SAMPLES
 
+// ? RANDOMISE 8 SAMPLES -------------------------------------------------
+// ! NOT WORKING...
 
-const randomiseEight = () => {
+// const randomiseEight = () => {
     
-    for(i = 0; i<8; i++) {
-        audioPlayer.src = daftSound[(Math.floor(Math.random() * 16))].soundToPlay
-        audioPlayer.play()
-        if (i < 8) {
-            setTimeout(500);
-        }
-    // * NEED FUNCTION TO PAUSE / WAIT WITHIN LOOP
-    }
-}
+//     for(i = 0; i<8; i++) {
+//         audioPlayer.src = daftSound[(Math.floor(Math.random() * 16))].soundToPlay
+//         audioPlayer.play()
 
-randomiseButton = document.querySelector("#play-8")
-randomiseButton.addEventListener("click", randomiseEight)
+    // * NEED FUNCTION TO PAUSE / WAIT WITHIN LOOP - ERROR WITH PROMISES
+//     }
+// }
+
+// randomiseButton = document.querySelector("#play-8")
+// randomiseButton.addEventListener("click", randomiseEight)
 
 
 
@@ -68,20 +87,20 @@ randomiseButton.addEventListener("click", randomiseEight)
 
 const daftSound = [
     {idObject: "#work_it", soundToPlay: "./sounds/work_it.wav"},
-    {idObject: "#make_it", soundToPlay: "./sounds/make_it.wav"},
-    {idObject: "#do_it", soundToPlay: "./sounds/do_it.wav"},
-    {idObject: "#makes_us", soundToPlay: "./sounds/makes_us.wav"},
     {idObject: "#harder", soundToPlay: "./sounds/harder.wav"},
+    {idObject: "#make_it", soundToPlay: "./sounds/make_it.wav"},
     {idObject: "#better", soundToPlay: "./sounds/better.wav"},
+    {idObject: "#do_it", soundToPlay: "./sounds/do_it.wav"},
     {idObject: "#faster", soundToPlay: "./sounds/faster.wav"},
+    {idObject: "#makes_us", soundToPlay: "./sounds/makes_us.wav"},
     {idObject: "#stronger", soundToPlay: "./sounds/stronger.wav"},
     {idObject: "#more_than", soundToPlay: "./sounds/more_than.wav"},
-    {idObject: "#hour", soundToPlay: "./sounds/hour.wav"},
-    {idObject: "#our", soundToPlay: "./sounds/our.wav"},
-    {idObject: "#never", soundToPlay: "./sounds/never.wav"},
     {idObject: "#ever", soundToPlay: "./sounds/ever.wav"},
+    {idObject: "#hour", soundToPlay: "./sounds/hour.wav"},
     {idObject: "#after", soundToPlay: "./sounds/after.wav"},
+    {idObject: "#our", soundToPlay: "./sounds/our.wav"},
     {idObject: "#work_is", soundToPlay: "./sounds/work_is.wav"},
+    {idObject: "#never", soundToPlay: "./sounds/never.wav"},
     {idObject: "#over", soundToPlay: "./sounds/over.wav"}
 ]
 
